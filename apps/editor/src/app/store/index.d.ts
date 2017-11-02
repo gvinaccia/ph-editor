8a70deb325c1a4ca5438303b75e4a6c55e62f170
Giovanni Vinaccia <vinaccia.giovanni@gmail.com>
2017-11-02 18:50:25 +0100

    sync con localstorage

--- apps/editor/src/app/store/index.d.ts
+++ apps/editor/src/app/store/index.d.ts
@@ -1,6 +1,22 @@
+declare interface IVector {
+  x: number;
+  y: number;
+}
+
+declare interface ISprite {
+  id: string | number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  src: string;
+  sprite_id: string;
+  scale: IVector;
+}
+
 declare interface AppState {
   assets: {
     images: any[]
   };
-  sprites: any[];
+  sprites: ISprite[];
 }
--- apps/editor/src/app/store/store.module.ts
+++ apps/editor/src/app/store/store.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StoreModule as NGRXStoreModule } from '@ngrx/store';
+import {ActionReducer, MetaReducer, StoreModule as NGRXStoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
+import {localStorageSync} from "ngrx-store-localstorage";
 import {assetsReducer} from "./reducers/assets";
 import {spritesReducer} from "./reducers/sprites";
 
 import 'rxjs/add/operator/filter';
 
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({keys: ['sprites','assets'], rehydrate: true})(reducer);
+}
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
+
 @NgModule({
   imports: [
     CommonModule,
-    NGRXStoreModule.forRoot({assets: assetsReducer, sprites: spritesReducer}),
+    NGRXStoreModule.forRoot({assets: assetsReducer, sprites: spritesReducer}, {metaReducers}),
     StoreDevtoolsModule.instrument(),
   ],
   declarations: []
