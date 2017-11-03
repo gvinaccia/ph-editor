86b231ac0e8a6d8b1989f54754dd28136ac211b7
Giovanni Vinaccia <vinaccia.giovanni@gmail.com>
2017-11-03 19:24:14 +0100

    wip: propertylist

--- apps/editor/src/app/sprite-properties/sprite-properties.component.ts
+++ apps/editor/src/app/sprite-properties/sprite-properties.component.ts
@@ -9,22 +9,21 @@ export class SpritePropertiesComponent implements OnInit {
 
   @Input() sprite: ISprite;
 
-  constructor() {}
-
   get props() {
-    return [];
-    /*
     if (! this.sprite) {
       return [];
     }
-    const ret = [];
 
-    Object.keys(this.sprite).forEach(key => {
-      ret.push({key, val: this.sprite[key]});
-    });
+    return Object.keys(this.sprite)
+      .filter(key => ['src'].indexOf(key) === -1)
+      .reduce((acc, key) => {
+        const rawVal = this.sprite[key];
+        const val = typeof rawVal === 'object'
+          ? `{x: ${rawVal.x.toFixed(2)}, y: ${rawVal.y.toFixed(2)}}`
+          : rawVal;
 
-    return ret;
-    */
+        return [...acc, {key, val}];
+      }, []);
   }
 
   ngOnInit() {}
