5c272a5472c5e20d434959b4b942e8eb173d990e
Giovanni Vinaccia <vinaccia.giovanni@gmail.com>
2017-11-03 19:08:17 +0100

    refactor: rimuove duplicazione dal sprites reducer

--- apps/editor/src/app/store/reducers/sprites.ts
+++ apps/editor/src/app/store/reducers/sprites.ts
@@ -1,36 +1,32 @@
+function updateSpriteInIndex(idToFind: number | string, sprites: ISprite[], cb: (ISprite) => ISprite) {
+  const index = sprites.findIndex(sp => sp.id === idToFind);
+  const updatedSprite = cb(sprites[index]);
+
+  return [
+    ...sprites.slice(0, index),
+    updatedSprite,
+    ...sprites.slice(index + 1)
+  ];
+}
+
 export function spritesReducer(state = [], action) {
   const { type, payload } = action;
 
   switch (type) {
     case 'STAGE.ADD_SPRITE':
       return [...state, payload];
-    case 'STAGE.SPRITE_MOVE': {
-      const index = state.findIndex(sp => sp.id === payload.sprite_id);
-      const updatedSprite = { ...state[index], x: payload.x, y: payload.y };
-
-      console.log('index', index);
-
-      return [
-        ...state.slice(0, index),
-        updatedSprite,
-        ...state.slice(index + 1)
-      ];
-    }
-    case 'STAGE.SPRITE_SCALE': {
-      const index = state.findIndex(sp => sp.id === payload.sprite_id);
-      if (index === -1) {
-        return state;
-      }
-      const updatedSprite = {...state[index], scale: payload.scale};
-
-      console.log('index', index);
-
-      return [
-        ...state.slice(0, index),
-        updatedSprite,
-        ...state.slice(index + 1)
-      ];
-    }
+    case 'STAGE.SPRITE_MOVE':
+      return updateSpriteInIndex(
+        payload.sprite_id,
+        state,
+        sprite => ({ ...sprite, x: payload.x, y: payload.y })
+      );
+    case 'STAGE.SPRITE_SCALE':
+      return updateSpriteInIndex(
+        payload.sprite_id,
+        state,
+        sprite => ({ ...sprite, scale: payload.scale })
+      );
     default:
       return state;
   }
